9b89dfa03703c2e0df67e762ad24ef01099beafa
강민채 <greenpea@gangminchaeui-MacBookPro.local>
2021-06-13 13:42:30 +0900

    master branch check

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -7,6 +7,7 @@ import org.apache.tomcat.util.buf.Utf8Encoder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
