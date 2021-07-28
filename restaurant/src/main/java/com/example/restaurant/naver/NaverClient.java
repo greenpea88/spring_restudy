ded78542653ac01f93800fbecc6ecc93ec144fe9
강민채 <greenpea@gangminchaeui-MacBookPro.local>
2021-07-28 20:56:26 +0900

    naver api를 이용한 wish list restaurant page 만들기

---
+++ restaurant/src/main/java/com/example/restaurant/naver/NaverClient.java
@@ -0,0 +1,81 @@
+package com.example.restaurant.naver;
+
+import com.example.restaurant.naver.dto.SearchImageReq;
+import com.example.restaurant.naver.dto.SearchImageRes;
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.naver.dto.SearchLocalRes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+public class NaverClient {
+
+    @Value("${naver.client.id}") //yml에 작성해놓은 값을 가지고 옮
+    private String naverClientId;
+    @Value("${naver.client.secret}")
+    private String naverClientSecret;
+
+    @Value("${naver.url.search.local}")
+    private String naverLocalUrl;
+    @Value("${naver.url.search.image}")
+    private String naverImageSearchUrl;
+
+    public SearchLocalRes localSearch(SearchLocalReq req){
+        URI uri = UriComponentsBuilder.fromUriString(naverLocalUrl)
+                .queryParams(req.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id",naverClientId);
+        headers.set("X-Naver-Client-Secret",naverClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchLocalRes>(){};
+
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+
+        return responseEntity.getBody();
+    }
+
+    public SearchImageRes searchImage(SearchImageReq req){
+        URI uri = UriComponentsBuilder.fromUriString(naverImageSearchUrl)
+                .queryParams(req.toMultiValueMap())
+                .build()
+                .encode()
+                .toUri();
+
+        var headers = new HttpHeaders();
+        headers.set("X-Naver-Client-Id",naverClientId);
+        headers.set("X-Naver-Client-Secret",naverClientSecret);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        var httpEntity = new HttpEntity<>(headers);
+        var responseType = new ParameterizedTypeReference<SearchImageRes>(){};
+
+        var responseEntity = new RestTemplate().exchange(
+                uri,
+                HttpMethod.GET,
+                httpEntity,
+                responseType
+        );
+
+        return responseEntity.getBody();
+    }
+}
---
+++ restaurant/src/main/java/com/example/restaurant/wishList/service/WishListService.java
@@ -0,0 +1,114 @@
+package com.example.restaurant.wishList.service;
+
+import com.example.restaurant.naver.NaverClient;
+import com.example.restaurant.naver.dto.SearchImageReq;
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.wishList.dto.WishListDto;
+import com.example.restaurant.wishList.entity.WishListEntity;
+import com.example.restaurant.wishList.repository.WishListRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class WishListService {
+
+    private final NaverClient naverClient;
+    private final WishListRepository wishListRepository;
+
+    public WishListDto search(String query){
+
+        //지역 검색
+        var searchLocalReq = new SearchLocalReq();
+        searchLocalReq.setQuery(query);
+
+        var searchLocalRes = naverClient.localSearch(searchLocalReq);
+
+        if(searchLocalRes.getTotal() > 0){
+            var localItem = searchLocalRes.getItems().stream().findFirst().get();
+            var imageQuery = localItem.getTitle().replaceAll("<[^>]*>","");
+            var searchImageReq = new SearchImageReq();
+            searchImageReq.setQuery(imageQuery);
+
+            //이미지 검색
+            var searchImageRes = naverClient.searchImage(searchImageReq);
+
+            //결과 return
+            if(searchImageRes.getTotal() > 0){
+                var imageItem = searchImageRes.getItems().stream().findFirst().get();
+
+                var result = new WishListDto();
+                result.setTitle(localItem.getTitle());
+                result.setCategory(localItem.getCategory());
+                result.setAddress(localItem.getAddress());
+                result.setRoadAddress(localItem.getRoadAddress());
+                result.setHomePageLink(localItem.getLink());
+                result.setImageLink(imageItem.getThumbnail());
+
+                return result;
+            }
+        }
+        return new WishListDto();
+    }
+
+    public WishListDto add(WishListDto wishListDto) {
+        var entity = dtoToEntity(wishListDto);
+        var saveEntity = wishListRepository.save(entity);
+        return entityToDto(saveEntity);
+    }
+
+    private WishListEntity dtoToEntity(WishListDto wishListDto){
+        var entity = new WishListEntity();
+        entity.setIndex(wishListDto.getIndex());
+        entity.setTitle(wishListDto.getTitle());
+        entity.setCategory(wishListDto.getCategory());
+        entity.setAddress(wishListDto.getAddress());
+        entity.setRoadAddress(wishListDto.getRoadAddress());
+        entity.setHomePageLink(wishListDto.getHomePageLink());
+        entity.setImageLink(wishListDto.getImageLink());
+        entity.setVisit(wishListDto.isVisit());
+        entity.setVisitCnt(wishListDto.getVisitCnt());
+        entity.setLastVisitDate(wishListDto.getLastVisitDate());
+
+        return entity;
+    }
+
+    private WishListDto entityToDto(WishListEntity wishListEntity){
+        var dto = new WishListDto();
+        dto.setIndex(wishListEntity.getIndex());
+        dto.setTitle(wishListEntity.getTitle());
+        dto.setCategory(wishListEntity.getCategory());
+        dto.setAddress(wishListEntity.getAddress());
+        dto.setRoadAddress(wishListEntity.getRoadAddress());
+        dto.setHomePageLink(wishListEntity.getHomePageLink());
+        dto.setImageLink(wishListEntity.getImageLink());
+        dto.setVisit(wishListEntity.isVisit());
+        dto.setVisitCnt(wishListEntity.getVisitCnt());
+        dto.setLastVisitDate(wishListEntity.getLastVisitDate());
+
+        return dto;
+    }
+
+    public List<WishListDto> findAll() {
+        return wishListRepository.listAll()
+                .stream()
+                .map(it -> entityToDto(it))
+                .collect(Collectors.toList());
+    }
+
+    public void delete(int index) {
+        wishListRepository.deleteById(index);
+    }
+
+    public void addVisit(int index){
+        var wishItem = wishListRepository.findById(index);
+        if(wishItem.isPresent()){
+            var item = wishItem.get();
+            item.setVisit(true);
+            item.setVisitCnt(item.getVisitCnt()+1);
+        }
+    }
+}
